1a20bcf563f41555f22173308ad5ab22c3728870
phyber <phyber@localhost>
2016-11-02 00:22:07 +0000

    crontab script now only needs /bin/sh

--- crontab-tweetdelete.sh
+++ crontab-tweetdelete.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 # Small script to quickly crontab tweetdelete.py if it's in a VirtualEnv.
 # This script will cd into and activate the VirtualEnv before executing
 # tweetdelete.py
 VIRTUALENV_DIR="${0%/*}"
 cd "${VIRTUALENV_DIR}"
-source activate
+. activate
 tweetdelete.py
